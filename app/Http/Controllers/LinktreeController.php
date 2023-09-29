f16fd46d609e7acdb206d615c50fed058832c91e
anjanidwilestari <ayianjani774@gmail.com>
2023-09-29 09:36:34 +0700

    php artisan make:model Linktree -msc --resource

---
+++ app/Http/Controllers/LinktreeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Linktree;
+use Illuminate\Http\Request;
+
+class LinktreeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Linktree $linktree)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Linktree $linktree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Linktree $linktree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Linktree $linktree)
+    {
+        //
+    }
+}
